d079c0908b8e7056e1ab2d6e9c06d44ac129e9db
Kaivalya Nandurkar <kaivalya.nandurkar2@gmail.com>
2021-09-13 23:24:21 +0530

    style: made changes in the styling

--- app.js
+++ app.js
@@ -1,10 +1,34 @@
 
 var btnTranslate = document.querySelector("#btn-translate");
-var txtInput =document.querySelector("#txt-input")
+var txtInput =document.querySelector("#txt-input");
+var outputDiv=document.querySelector("#output");
 
-console.log(txtInput)
+ var serverURL = "https://api.funtranslations.com/translate/minion.json"
 
-btnTranslate.addEventListener("click", function clickEventHandler(){
-    console.log("clicked!")
-    console.log("input", txtInput.value)
-} )
+function getTranslationURL(input){
+    return serverURL + "?" + "text=" + input
+}
+
+function errorHandler(error) {
+    console.log("error occured", error);
+    alert("Something wrong with server! Try again after some time")
+}
+
+
+
+function clickHandler(){
+    
+    var inputText = txtInput.value; //taking input 
+
+    //Calling server for processing
+    fetch(getTranslationURL(inputText)) 
+    .then(response=>response.json())
+    .then(json => {
+        var translatedText = json.contents.translated;
+        outputDiv.innerText = translatedText;
+    })
+    .catch(errorHandler)
+
+};
+
+btnTranslate.addEventListener("click", clickHandler)
